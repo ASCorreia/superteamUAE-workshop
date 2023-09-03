b9e6d9432d2685a58870a7d2ed2301b12d74a378
André Correia <andresilvacorreia@gmail.com>
2023-09-03 17:32:49 +0100

    Counter account initialization done

--- programs/solana-dev-workshop/src/contexts/counter.rs
+++ programs/solana-dev-workshop/src/contexts/counter.rs
@@ -1,1 +1,22 @@
 use crate::*;
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 1)]
+    pub account: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Initialize<'info> {
+    pub fn initialize(&mut self) -> Result<()> {
+        self.account.counter = 0;
+
+        Ok(())
+    }
+}
+
+#[account]
+pub struct Counter {
+    pub counter: u8,
+}
--- tests/solana-dev-workshop.ts
+++ tests/solana-dev-workshop.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { SolanaDevWorkshop } from "../target/types/solana_dev_workshop";
+import { Keypair, LAMPORTS_PER_SOL, SystemProgram } from "@solana/web3.js";
 
 describe("solana-dev-workshop", () => {
   // Configure the client to use the local cluster.
@@ -9,9 +10,29 @@ describe("solana-dev-workshop", () => {
 
   const program = anchor.workspace.SolanaDevWorkshop as Program<SolanaDevWorkshop>;
 
+  const userKeypair = Keypair.generate();
+
+  it("Request airdrop", async () => {
+    let airdropTx = await provider.connection.requestAirdrop(userKeypair.publicKey, 2 * LAMPORTS_PER_SOL);
+    let latestBlockHash = await provider.connection.getLatestBlockhash();
+    
+    await provider.connection.confirmTransaction({
+      blockhash: latestBlockHash.blockhash,
+      lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+      signature: airdropTx,
+    });
+
+    console.log("\n\nAirdrop Operation Successfull");
+  });
+
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const tx = await program.methods.initialize().accounts({
+      account: userKeypair.publicKey,
+      user: provider.publicKey,
+      systemProgram: SystemProgram.programId
+    }).signers([userKeypair]).rpc();
+
+    console.log("\n\nCounter account succesfully initialized! TxID: ", tx);
   });
 });
